1b8761bc7c2096cef17865097c8a560fe8d2123d
CristopherNSerrano <cristopherserranoutsa@gmail.com>
2025-01-27 15:14:10 -0600

    I f up

--- MobileApp2/app/(tabs)/index.tsx
+++ MobileApp2/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello Jucie World!! cris</ThemedText>
+        <ThemedText type="title">Hello Jucie World!! crishhh</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
