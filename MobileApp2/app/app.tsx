b362fe6f46bf1b90771646b049b55cfc0b464fae
7jmart <7jmart@gmail.com>
2025-01-28 14:09:39 -0600

    a

--- MobileApp2/app/app.tsx
+++ MobileApp2/app/app.tsx
@@ -13,7 +13,7 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <AuthNavigator />
+      <AuthNavigator /> 
     </NavigationContainer>
   );
 };
