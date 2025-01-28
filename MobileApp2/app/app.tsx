d7d274154674c9b26f10efefbbb1babdfe647cdf
CristopherNSerrano <cristopherserranoutsa@gmail.com>
2025-01-28 11:53:40 -0600

    Added Detail

--- MobileApp2/app/app.tsx
+++ MobileApp2/app/app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeViewGestureHandler } from 'react-native-gesture-handler';
 import Index from './(tabs)/index';  // Your main screen
 import LoginPage from './(tabs)/LoginPage'; // Login screen
 
@@ -8,12 +10,14 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Index">
-        <Stack.Screen name="Index" component={Index} />
-        <Stack.Screen name="Login" component={LoginPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NativeViewGestureHandler>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Index">
+          <Stack.Screen name='Index' component={Index} />
+          <Stack.Screen name='Login' component={LoginPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeViewGestureHandler>
   );
 };
 
